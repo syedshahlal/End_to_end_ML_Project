7c9f1bdf8ca16c359ac1fa41c00c1f266fdd42a8
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-09-15 13:14:55 -0400

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return 'CI/CD pipeline with Heroku and Github has been setup successfully!'
+    return 'CI/CD pipeline with Heroku and Github has been setup successfully! This is a simple housing price predictor app'
 
 if __name__ == '__main__':
     app.run(debug=True)
