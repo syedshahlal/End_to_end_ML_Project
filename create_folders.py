b5135b11cf582646b3379ae4588c27d5818eb921
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-09-16 05:16:23 -0400

    Added packages in the housing

---
+++ create_folders.py
@@ -0,0 +1,17 @@
+import os
+
+# Create a folder
+dir = os.getcwd()
+
+target_folder = os.path.join(dir, 'housing')
+
+# print(target_folder)
+
+create_folders = ['exception','logger','pipeline', 'component','config','entity']
+for folder in create_folders:
+    path = os.path.join(target_folder, folder)
+    # print(path)
+    if not os.path.exists(path):  # Check if the path does not exist
+        os.mkdir(path)
+        with open(os.path.join(path, '__init__.py'), 'w') as f:
+            pass
