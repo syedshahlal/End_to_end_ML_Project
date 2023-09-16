2c3bcd263c368c03687a03cf2596bf5803b076d9
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-09-16 10:54:07 -0400

    updated logging.py

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -10,9 +10,11 @@ LOG_FILE_NAME = f"log_{CURRENT_TIME_STAMP}.log"
 
 os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME,
-                             filemode = 'w',
-                             format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-                             level = logging.INFO                     
-                             )
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+logging.basicConfig(filename = LOG_FILE_PATH,
+                    filemode = 'w',
+                    format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+                    level = logging.INFO                     
+                    )
 
