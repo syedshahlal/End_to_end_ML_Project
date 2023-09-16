4d2fc42339ab117a02105f244b5853ce50a42d6f
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-09-16 11:33:32 -0400

    logging working

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
     logging.info("Main page is accessed")
-    return 'CI/CD pipeline with Heroku and Github has been setup successfully! This is a simple housing price predictor app' + ' Check logs'
+    return 'CI/CD pipeline with Heroku and Github has been setup successfully! This is a simple housing price predictor app' + '\n' + ' Check logs'
 
 if __name__ == '__main__':
     app.run(debug=True)
