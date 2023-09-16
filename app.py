f4c1888e34fa73a9fee977dc7e45bb484b4ee8a4
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-09-16 11:22:51 -0400

    changes in logger

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    logging.info('Main page is accessed')
-    return 'CI/CD pipeline with Heroku and Github has been setup successfully! This is a simple housing price predictor app'
+    logging.info("Main page is accessed")
+    return 'CI/CD pipeline with Heroku and Github has been setup successfully! This is a simple housing price predictor app' + ' Check logs'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,10 +1,10 @@
-import  logging
+import logging
 from datetime import datetime
 import os
 
 LOG_DIR = "housing_logs"
 
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
 LOG_FILE_NAME = f"log_{CURRENT_TIME_STAMP}.log"
 
@@ -13,8 +13,8 @@ os.makedirs(LOG_DIR, exist_ok=True)
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
 
 logging.basicConfig(filename = LOG_FILE_PATH,
-                    filemode = 'w',
-                    format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-                    level = logging.INFO                     
-                    )
+filemode = "w",
+format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+level = logging.INFO                     
+)
 
