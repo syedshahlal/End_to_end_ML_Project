272bafa1409ea3b8981293bef62e64f264c4c905
Syed Shahlal <sr.shahlal@gmail.com>
2023-09-23 11:50:52 -0400

    xyz

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -217,9 +217,9 @@ class Configuartion:
         try:
             training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
             artifact_dir = os.path.join(ROOT_DIR,
-            training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
-            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY]
-            )
+                                        training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+                                        training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY]
+                                        )
 
             training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
             logging.info(f"Training pipleine config: {training_pipeline_config}")
